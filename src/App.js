2bc8113100973634dbcb5a00fd16a7aa82327c53
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2022-09-16 17:29:00 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display" id="display">
+          Display
+        </div>
+        <div className="buttons">
+          <div className="number-buttons">
+            <button id="clear">AC</button>
+            <button id="divide">/</button>
+            <button id="multiply">x</button>
+            <button id="subtract">-</button>
+            <button id="add">+</button>
+            <button id="equals">=</button>
+            <button>0</button>
+            <button id="decimal">.</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+{
+  /* <div className="control-buttons">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+          </div> */
+}
