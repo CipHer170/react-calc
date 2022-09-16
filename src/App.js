7862f094f41fd611f8ad55eaf8784f6ca3cbd132
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2022-09-16 17:31:08 +0500

    calc version1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const clearData = () => {
+    setData([]);
+  };
+
   return (
     <div className="App">
       <div className="calculator">
         <div className="display" id="display">
-          Display
+          {data}
         </div>
+
         <div className="buttons">
           <div className="number-buttons">
-            <button id="clear">AC</button>
+            <button id="clear" onClick={(e) => clearData(e)}>
+              AC
+            </button>
             <button id="divide">/</button>
             <button id="multiply">x</button>
             <button id="subtract">-</button>
             <button id="add">+</button>
             <button id="equals">=</button>
-            <button>0</button>
+            <button id="zero">0</button>
             <button id="decimal">.</button>
+            <span className="numeric">
+              <button id="one">1</button>
+              <button id="two">2</button>
+              <button id="three">3</button>
+              <button id="four">4</button>
+              <button id="five">5</button>
+              <button id="six">6</button>
+              <button id="seven">7</button>
+              <button id="eight">8</button>
+              <button id="nine">10</button>
+            </span>
           </div>
         </div>
       </div>
@@ -26,15 +47,4 @@ function App() {
 
 export default App;
 {
-  /* <div className="control-buttons">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-          </div> */
 }
